b5b976c033c6ffb81c65e5e15de844fe2bfdf1c4
cescarsoporte <cescar.soporte@gmail.com>
2024-11-26 10:19:31 -0400

    Olimiada de espanole y sociales de 5to y 6to update 1

--- src/components/OlimpSec.js
+++ src/components/OlimpSec.js
@@ -34,7 +34,8 @@ const OlimpSec = () => {
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE lengua española, categoría A: (5to y 6to de primaria).">CATEG. A</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE lengua española, categoría A: (5to y 6to de primaria).">CATEG. A 6TO</a></li><p />
+							<li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE lengua española, categoría A: (5to y 6to de primaria).">CATEG. A 5TO</a></li><p />
                         </ul>
                     </div>
                     <div className="col-1 col-sm-2 secundaria shadow w-auto">
@@ -59,7 +60,8 @@ const OlimpSec = () => {
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE quimica, categoría A: (5to y 6to de primaria).">CATEG. A</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE lengua española, categoría A: (5to y 6to de primaria).">CATEG. A 6TO</a></li><p />
+							<li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE lengua española, categoría A: (5to y 6to de primaria).">CATEG. A 5TO</a></li><p />
                         </ul>
                     </div>
 					<div className="col-1 col-sm-2 secundaria shadow w-auto">
@@ -71,7 +73,8 @@ const OlimpSec = () => {
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE fisica, categoría A: (5to y 6to de primaria).">CATEG. A</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE lengua española, categoría A: (5to y 6to de primaria).">CATEG. A 6TO</a></li><p />
+							<li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE lengua española, categoría A: (5to y 6to de primaria).">CATEG. A 5TO</a></li><p />
                         </ul>
                     </div>
                 </div>
