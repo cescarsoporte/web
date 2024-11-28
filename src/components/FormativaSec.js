50582ff1dd8ff571649066ba7de1244d8e4c9de4
cescarsoporte <cescar.soporte@gmail.com>
2024-11-28 13:18:20 -0400

    Evaluacion formativa de Ciencias Naturales y Ciencias Sociales de primaria

--- src/components/FormativaSec.js
+++ src/components/FormativaSec.js
@@ -30,9 +30,9 @@ const FormativaSec = () => {
                     <div className="col-1 col-sm-2 secundaria shadow w-auto">
                         <p>NATURALES</p>
                         <ul className="list-unstyled">
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">5TO</a></li><p />
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://forms.gle/wyxcbNr1DPZWLUov9" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://forms.gle/M5A5qZXdwPQbXSfu9" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">5TO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://forms.gle/ah2n7vBhp1schfXf8" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS NATURALES-CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
@@ -42,7 +42,7 @@ const FormativaSec = () => {
                     <div className="col-1 col-sm-2 secundaria shadow w-auto">
                         <p>SOCIALES</p>
                         <ul className="list-unstyled">
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS SOCIALES-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://forms.gle/K76UGp941SoWQz6o9" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS SOCIALES-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS SOCIALES-CENTRO EDUCATIVO SANTO CURA DE ARS">5TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS SOCIALES-CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EDUCACION FORMATIVA DE CIENCIAS SOCIALES-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
