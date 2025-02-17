33a6bc305562f129d3e19185363bef100c3bffa5
cescarsoporte <cescar.soporte@gmail.com>
2025-02-17 09:08:27 -0400

    Olimniada de ingles 2

--- src/components/OlimpSec.js
+++ src/components/OlimpSec.js
@@ -19,8 +19,8 @@ const OlimpSec = () => {
                         <p>MATEMATICA</p>
                         <ul className="list-unstyled">
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE MATEMATICA-CENTRO EDUCATIVO SANTO CURA DE ARS">CATEG. D</a></li><p />
-                            <li><a className="btn btn-primary shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLScaMYA1uPf8y4UDCselJqHdZG9ekh8jZbJZzaO8a91fhCPKMg/viewform" target="_blank" title="OLIMPIADA INTERNA DE MATEMATICA-CENTRO EDUCATIVO SANTO CURA DE ARS">CATEG. C</a></li><p />
-                            <li><a className="btn btn-primary shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLSdZTMCHGDTec__6udt_BGVK284Jk128qtH1v4jjhYV38aD34w/viewform?pli=1" target="_blank" title="OLIMPIADA INTERNA DE MATEMATICA-CENTRO EDUCATIVO SANTO CURA DE ARS">CATEG. B</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLScaMYA1uPf8y4UDCselJqHdZG9ekh8jZbJZzaO8a91fhCPKMg/viewform" target="_blank" title="OLIMPIADA INTERNA DE MATEMATICA-CENTRO EDUCATIVO SANTO CURA DE ARS">CATEG. C</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLSdZTMCHGDTec__6udt_BGVK284Jk128qtH1v4jjhYV38aD34w/viewform?pli=1" target="_blank" title="OLIMPIADA INTERNA DE MATEMATICA-CENTRO EDUCATIVO SANTO CURA DE ARS">CATEG. B</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLSe9G5FCcQj-y2FtybU-dnch0tqf0rkDmogDnGmyqHYNgplcTw/viewform" target="_blank" title="OLIMPIADA INTERNA DE matematica, categoría A: (5to y 6to de primaria).">CATEG. A</a></li><p />
                         </ul>
                     </div>
@@ -51,9 +51,9 @@ const OlimpSec = () => {
 					<div className="col-1 col-sm-2 secundaria shadow w-auto">
                         <p>ENGLISH</p>
                         <ul className="list-unstyled">
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLSfehBQnGgRnN4JvPpwzvQl9-8ZzoseQQMnrPaKvb1u7V8woJQ/viewform?usp=header" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">5TO</a></li><p />
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLScLco3IsRMgh0PR_HIaPo3QU5kP9myfFEms4nhX-Mz9k-NcCQ/viewform?usp=sharing" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="OLIMPIADA INTERNA DE INGLES-CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
