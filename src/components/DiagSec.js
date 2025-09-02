1f765a91198233a81eab01e88d1b3ae4a54038a1
unknown <Administrator@CESCAR.LOCAL>
2025-09-02 05:36:57 -0700

    Leo FIHR

--- src/components/DiagSec.js
+++ src/components/DiagSec.js
@@ -54,7 +54,7 @@ const DiagSec = () => {
 					<div className="col-1 col-sm-2 secundaria shadow w-auto">
                         <p>QUIMICA</p>
                         <ul className="list-unstyled">
-                            <li><a className="btn btn-primary disabled shadow w-100" href="https://es.educaplay.com/recursos-educativos/24941684-quiz_de_quimica_conceptos_clave.html" target="_blank" title="EVALUACION DIAGNOSTICA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
                             <li><a className="btn btn-primary shadow w-100" href="https://forms.gle/P7Z4CbS8Kqr2Suj48" target="_blank" title="EVALUACION DIAGNOSTICA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">5TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE QUIMICA-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
@@ -69,7 +69,7 @@ const DiagSec = () => {
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">5TO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
-                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://es.educaplay.com/recursos-educativos/24941684-quiz_de_quimica_conceptos_clave.html" target="_blank" title="EVALUACION DIAGNOSTICA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE FISICA-CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="Evaluacion diagnostica de fisica, categoría A: (5to y 6to de primaria).">CATEG. A</a></li><p />
@@ -85,6 +85,18 @@ const DiagSec = () => {
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
                             <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="Evaluacion diagnostica de ingles, categoría A: (5to y 6to de primaria).">CATEG. A</a></li><p />
+                        </ul>
+                    </div>
+					<div className="col-1 col-sm-2 secundaria shadow w-auto">
+                        <p>FIHR</p>
+                        <ul className="list-unstyled">
+                            <li><a className="btn btn-primary shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLSdbRN_yO3WWTMLCn6KjIHUNPS4qMpRgDj4dOOgK1TyjOAp9eQ/viewform?usp=dialog" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">6TO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLSenKvaOip4D83zeaCzuzyBS1ZSs9F9itLBqdCokx-4S93JRRg/viewform?usp=dialog" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">5TO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLScZK1OeLbkdtSm2d5YVTEJr99QOIkk0sprP7Us-eOe5jE-Nag/viewform?usp=dialog" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">4TO</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">3RO</a></li><p />
+                            <li><a className="btn btn-primary shadow w-100" href="https://docs.google.com/forms/d/e/1FAIpQLSfGofz-dWw-HdtaojYJr7HTJejwE9MKSdSAPiMPLges6CEMEQ/viewform?usp=dialog" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">2DO</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="EVALUACION DIAGNOSTICA DE INGLES CENTRO EDUCATIVO SANTO CURA DE ARS">1RO</a></li><p />
+                            <li><a className="btn btn-primary disabled shadow w-100" href="#" target="_blank" title="Evaluacion diagnostica de Formacion Integral Humano y Religiosa, categoría A: (5to y 6to de primaria).">CATEG. A</a></li><p />
                         </ul>
                     </div>
                 </div>
